1873a64bf4558e6eea2d1121d2b058e9540ecffb
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-14 15:55:39 +0200

    remove console-check as it failes in testing

--- Providers/MenuServiceProvider.php
+++ Providers/MenuServiceProvider.php
@@ -106,7 +106,7 @@ class MenuServiceProvider extends ServiceProvider
      */
     private function defineMenuBuilder()
     {
-        if (! $this->app['society.isInstalled'] || App::runningInConsole()) {
+        if (! $this->app['society.isInstalled']) {
             return;
         }
 
